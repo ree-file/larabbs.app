03ee8cd9c00e0c90659ec2c0cb95a464b594a049
ree_file <1205730728@qq.com>
2017-11-15 13:20:29 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Summer</a>设计编码 <span style="color:#e27575;font-size:14px">❤</span>
+        </p>
+        <p class="pull-right"> <a href="mailto:name@eamil.com"></a>联系我们 </p>
+    </div>
+</footer>
